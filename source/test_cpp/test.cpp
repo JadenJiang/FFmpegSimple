56f6c0b4f4b10207d130892106694835d8988edb
Jaden <jiangjie618@outlook.com>
2018-05-25 18:14:22 +0800

    audio resample

--- source/test_cpp/test.cpp
+++ source/test_cpp/test.cpp
@@ -1,7 +1,18 @@
 #include "test.h"
 #include <iostream>
-
+#include <thread>
+class Test {
+public:
+    Test() {
+        printf("Test()\n");
+    }
+    ~Test() {
+        printf("~Test()\n");
+    }
+};
 int main() {
+    std::unique_ptr<Test> a;
+    a = std::unique_ptr<Test>(new Test());
 	std::cout << "1111\n";
 	return 0;
 }
